f0ed645b525bf92c9a185c65d6b86aad05559286
Tony Maina <tony.maina@bambagroup.com>
2017-09-11 16:10:51 +0300

    customization

--- app/src/main/java/com/github/javiersantos/appupdater/demo/MainActivity.java
+++ app/src/main/java/com/github/javiersantos/appupdater/demo/MainActivity.java
@@ -1,53 +1,72 @@
 package com.github.javiersantos.appupdater.demo;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.AppUpdaterUtils;
 import com.github.javiersantos.appupdater.demo.databinding.ActivityMainBinding;
-import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.AppUpdaterError;
 import com.github.javiersantos.appupdater.enums.UpdateFrom;
+import com.github.javiersantos.appupdater.objects.Update;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    private Context context;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        this.context = this;
+
 
         setSupportActionBar(binding.toolbar);
 
-        binding.included.notificationUpdate.setOnClickListener(new View.OnClickListener() {
+
+        AppUpdaterUtils appUpdaterUtils = new AppUpdaterUtils(this)
+//                .setUpdateFrom(UpdateFrom.AMAZON)
+                .setUpdateFrom(UpdateFrom.GITHUB)
+                .setGitHubUserAndRepo("TontNais", "AppUpdater")
+                //...
+                .withListener(new AppUpdaterUtils.UpdateListener() {
+                    @Override
+                    public void onSuccess(Update update, Boolean isUpdateAvailable) {
+                        Log.d("Latest Version", update.getLatestVersion());
+                        Log.d("Latest Version Code", String.valueOf(update.getLatestVersionCode()));
+                        Log.d("Release notes", update.getReleaseNotes());
+                        Log.d("URL", String.valueOf(update.getUrlToDownload()));
+                        Log.d("Is update available?", Boolean.toString(isUpdateAvailable));
+                    }
+
+                    @Override
+                    public void onFailed(AppUpdaterError error) {
+                        Log.d("AppUpdater Error", "Something went wrong");
+                    }
+                });
+        appUpdaterUtils.start();
+
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new AppUpdater(context)
-                        //.setUpdateFrom(UpdateFrom.GITHUB)
-                        //.setGitHubUserAndRepo("javiersantos", "AppUpdater")
-                        .setUpdateFrom(UpdateFrom.JSON)
-                        .setUpdateXML("https://raw.githubusercontent.com/TontNais/AppUpdater/master/app/update.json")
-                        .setDisplay(Display.NOTIFICATION)
-                        .showAppUpdated(true)
-                        .start();
+                getUpdate();
+                //startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/TontNais/AppUpdater")));
             }
         });
 
-        /*binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/TontNais/AppUpdater")));
-            }
-        });
-
-        binding.included.dialogUpdateChangelog.setOnClickListener(new View.OnClickListener() {
+        /*binding.included.dialogUpdateChangelog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 new AppUpdater(context)
@@ -140,6 +159,54 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+
+
+    public void getUpdate(){
+
+        String destination = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/";
+        String fileName = "relay_app_726.apk";
+        destination += fileName;
+        final Uri uri = Uri.parse("file://" + destination);
+
+        //Delete update file if exists
+        File file = new File(destination);
+        if (file.exists())
+            //file.delete() - test this, I think sometimes it doesnt work
+            file.delete();
+
+        //get url of app on server
+        String url = "http://bambagroup.com/android_sms/relay_app/relay_app_726.apk";//this.getString(R.string.update_app_url);
+
+        //set downloadmanager
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription("description");//this.getString(R.string.notification_description));
+        request.setTitle(this.getString(R.string.app_name));
+
+        //set destination
+        request.setDestinationUri(uri);
+
+        // get download service and enqueue file
+        final DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        final long downloadId = manager.enqueue(request);
+
+        //set BroadcastReceiver to install app when .apk is downloaded
+        BroadcastReceiver onComplete = new BroadcastReceiver() {
+            public void onReceive(Context ctxt, Intent intent) {
+                Intent install = new Intent(Intent.ACTION_VIEW);
+                install.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                install.setDataAndType(uri,manager.getMimeTypeForDownloadedFile(downloadId));
+                install.setDataAndType(uri,
+                        "application/vnd.android.package-archive");
+                startActivity(install);
+
+                unregisterReceiver(this);
+                finish();
+            }
+        };
+        //register receiver for when .apk download is compete
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
 }
